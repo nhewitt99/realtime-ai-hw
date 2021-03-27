d99f00918b101cfa4b8c97f736457dc696881aba
nhewitt99 <nhewitt@uncc.edu>
2021-03-26 21:31:37 -0400

    Troubleshot pipeline and added COCO json file

--- hw3/hw3p3.py
+++ hw3/hw3p3.py
@@ -7,7 +7,7 @@ Homework 3, part 3
 
 Description: This file reads images from a webcam, uses YOLOv5 to identify
 people in the image, and then passes the people into a ResNet18 TensorRT
-model to perform pose estimation on the people. This pipeline is adapted
+model to perform pose estimation on the people. The main() fn is adapted
 from my work for Homework 1, part 3.
 """
 
@@ -28,47 +28,40 @@ from torchvision import transforms
 from PIL import Image
 
 def processFrame(frame, yolo, resnet, parseObjects, drawObjects):
+    # Get detections from YOLO
     detections = runYolo(frame, yolo)
+    
+    # Save the YOLO output frame
     frame = detections.render()[-1].copy()
 
+    # Find bboxes of people by checking the Detection class' label
     bboxes = getPeople(detections)
 
     if bboxes:
-        print(f'Found {len(bboxes)} people')
+        print(f'Found {len(bboxes)} people.')
         for b in bboxes:
-            # Grab pixels for bbox
-            personImg = frame[b[0]:b[2], b[1]:b[3]]
+            # Grab pixels from YOLO image for bbox
+            personImg = frame[b[1]:b[3], b[0]:b[2]]
 
-            img = runResNet(cv2.UMat(personImg), resnet, parseObjects, drawObjects)
-            img.detach().cpu()
+            # Render person's joints with ResNet and stack onto YOLO frame
+            img = runResNet(personImg, resnet, parseObjects, drawObjects)
             frame = combineFrame(frame, b, img)
-    return frame, (bboxes is None)
+            
+    # Convert back to BGR on return so cv.imwrite will work
+    return frame[:, :, ::-1], (bboxes is None)
 
 
 def runResNet(image, resnet, parseObjects, drawObjects):
-    # Transform input image into ResNet size
-    # Run ResNet on image
-    # Draw joints
-    # Transform output image back to input size
-    # Return image
-    try:
-        shape = [len(image), len(image[0])]
-        print(shape)
-    except Exception as e:
-        print(e)
-    image = preprocessResNet(image)
-    #cv2.imwrite('imgs/preproc.jpg', image.numpy().transpose(1, 2, 0))
+    # Preprocess according to demo
+    data = preprocessResNet(image)
 
-    cmap, paf = resnet(image)
+    # Run ResNet on image
+    cmap, paf = resnet(data)
     cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
     counts, objects, peaks = parseObjects(cmap, paf)
+    
+    # Draw joints
     drawObjects(image, counts, objects, peaks)
-
-    try:
-        image = transforms.functional.resize(image, shape)
-    except Exception as e:
-        print(e)
-    #cv2.imwrite('imgs/resnet.jpg', image.numpy().transpose(1, 2, 0))
 
     return image
 
@@ -79,33 +72,34 @@ def preprocessResNet(image):
     std = torch.Tensor([0.299, 0.224, 0.225]).cuda()
     device = torch.device('cuda')
 
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = Image.fromarray(image)
+    print(image)
     image = transforms.functional.to_tensor(image).to(device)
-    image.sub_(mean[:,None,None]).div_(std[:,None,None])
+    #image.sub_(mean[:,None,None]).div_(std[:,None,None])
     image = transforms.functional.resize(image, [224, 224])
     return image[None, ...]
 
 
 def combineFrame(frame, bbox, image):
-    frame[bbox[0]:bbox[2], bbox[1]:bbox[3]] = image
+    # Just replace pixels in frame of bounding box with image
+    frame[bbox[1]:bbox[3], bbox[0]:bbox[2]] = image
     return frame
 
 
 def runYolo(frame, yolo):
-    # Run yolo and return detections
+    # Run yolo and return detections. In hindsight, this didn't need a function.
     return yolo([frame])
 
 
 def getPeople(results):
     # Pull the people out of the detections from YOLO
-    # Return a List of their bboxes
     predictions = results.pred[-1]
 
+    # List comprehension to grab people, then those people's boxes
     people = [p for p in predictions if p[-1] == 0] # 0 is label for Person
     bboxes = [p[0:4].tolist() for p in people]
 
-    # Round everything to int
+    # List comp to round everything to int
     bboxes = [[round(i) for i in box] for box in bboxes]
     return bboxes
 
@@ -126,7 +120,7 @@ def main():
     topology = trt_pose.coco.coco_category_to_topology(human_pose)
 
     resnet = TRTModule()
-    resnet.load_state_dict(torch.load('densenet_trt.pth'))
+    resnet.load_state_dict(torch.load('resnet_trt.pth'))
 
     parseObjects = ParseObjects(topology)
     drawObjects = DrawObjects(topology)
@@ -138,10 +132,11 @@ def main():
     # Continue until interrupt
     try:
         while True:
-            # Grab as many frames as batch size
+            # Grab a frame
             img = cam.read()[:, :, ::-1] # Convert BGR to RGB
-            print('got frame')
-            # Process with yolo
+            print('got frame {imageCount}')
+            
+            # Process with yolo and resnet
             result, empty = processFrame(img, yolo, resnet, parseObjects, drawObjects)
 
             # Save file
